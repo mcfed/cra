07dd9cf9d49f66c5adce7c61a7c600fd3032712e
lixiang <lixianghz@mchz.com.cn>
2020-04-01 13:56:32 +0800

    fix: sss

--- bin/index.js
+++ bin/index.js
@@ -4,17 +4,19 @@ var program = require('commander')
 var fs = require('fs')
 var path = require('path')
 
-let package = fs.readFileSync(path.join(__dirname, '../package.json'), 'utf8')
-package = JSON.parse(package)
+const package = require('../package.json')
 
 program
   .version(package.version)
   .option('-c, --change-to-available-version', '安装可利用的create-react-app版本')
+  .option('-d, --diff-gitlab-wiki-markdown', '比较wiki下文档的历史提交变更')
   .action(async function (commander) {
     if (commander.changeToAvailableVersion) {
-      require('../install')
+      require('../src/create-project/install')
+    } else if (commander.diffGitlabWikiMarkdown) {
+      require('../src/gitdiff-wikis')
     } else {
-      require('../create')
+      require('../src/create-project/create')
     }
   })
   .parse(process.argv)
