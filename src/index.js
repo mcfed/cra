3ec83953b74bd003c287111ae1f81e2a868a53a5
李响 <>
2020-05-08 10:50:32 +0800

    feat: 0.0.7

--- src/index.js
+++ src/index.js
@@ -5,9 +5,10 @@ const paths = require('react-scripts/config/paths')
 const {
     disableChunk,
     adjustStyleLoaders,
+    addWebpackAlias,
 } = customizeCra
 
-customizeCra.addDefaultConfig = () => config => {
+customizeCra.prodDefaultConfig = () => config => {
     const modes = {
         cjs: 'cjs',
         umd: 'umd'
@@ -40,6 +41,49 @@ customizeCra.addDefaultConfig = () => config => {
         loader.use = loader.use.filter(it => it.loader.indexOf('mini-css-extract-plugin') === -1)
     })(config)
     disableChunk()(config)
+    
+    return config
+}
+
+customizeCra.devDefaultConfig = () => config => {
+    const isDev = process.env.NODE_ENV === 'development'
+    if (!isDev) { return config }
+    const app = path.join(paths.appPath, 'app.js')
+    const appIndex = path.join(paths.appPath, 'index.js')
+    
+    // 加别名
+    addWebpackAlias({
+      'react-intl': path.join(paths.appNodeModules, 'react-intl'),
+      'mcf-module': path.join(paths.appNodeModules, 'mcf-module')
+    })(config)
+  
+    // 增加入口文件
+    config.entry.push(appIndex)
+  
+    // 修改babel-loader的include
+    if (!config.module) {
+      config.module = {}
+    }
+    if (!config.module.rules || !config.module.rules.length) {
+      config.module.rules = []
+    }
+    for (const item of (config.module.rules.find(rule => Array.isArray(rule.oneOf)).oneOf || [])) {
+      if (item.loader && 
+          item.loader.indexOf('babel-loader') !== -1 && 
+          item.options &&
+          item.options.customize &&
+          item.options.customize.indexOf('babel-preset-react-app') !== -1
+        ) {
+          if (!item.include) {
+            item.include = []
+          }
+          if (!Array.isArray(item.include)) {
+            item.include = [item.include]
+          }
+          item.include.push(app)
+          item.include.push(appIndex)
+      }
+    }
     
     return config
 }
