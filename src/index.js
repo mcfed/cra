9c5d8bb4bcd05e394f81e329af91d4f00e0de702
李响 <>
2020-07-02 09:39:11 +0800

    fix: 增加babel的include的自定义

--- src/index.js
+++ src/index.js
@@ -136,4 +136,40 @@ customizeCra.customKeepFunctionNameConfig = () => config => {
   return config
 }
 
+customizeCra.customBabelLoaderInclude = (includePathArray=[]) => config => {
+  
+  if (!Array.isArray(includePathArray) || !includePathArray.length) {
+    return config
+  }
+
+  // 修改babel-loader的include
+  if (!config.module) {
+    config.module = {}
+  }
+  if (!config.module.rules || !config.module.rules.length) {
+    config.module.rules = []
+  }
+  for (const item of (config.module.rules.find(rule => Array.isArray(rule.oneOf)).oneOf || [])) {
+    if (item.loader && 
+        item.loader.indexOf('babel-loader') !== -1 && 
+        item.options &&
+        item.options.customize &&
+        item.options.customize.indexOf('babel-preset-react-app') !== -1
+      ) {
+        if (!item.include) {
+          item.include = []
+        }
+        if (!Array.isArray(item.include)) {
+          item.include = [item.include]
+        }
+        for (const babelIncluedPath of includePathArray) {
+          // const incluePath = path.resolve(paths.appPath, babelIncluedPath)
+          item.include.push(babelIncluedPath)
+        }
+    }
+  }
+  
+  return config
+}
+
 module.exports = customizeCra
