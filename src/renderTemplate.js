4be8fa6c0e8b1bb1e3aa8f9eb468f7038e3d6471
李响 <>
2020-07-14 09:27:55 +0800

    feat: 增加渲染模块功能

---
+++ src/renderTemplate.js
@@ -0,0 +1,28 @@
+const fs = require('fs-extra');
+const path = require('path');
+
+module.exports = function main(moudleName, templateDir, newRenderProjectDir) {
+    try {
+        if (!moudleName) {
+            moudleName = 'demoTemplate'
+        }
+
+        if (!templateDir) {
+            templateDir = path.resolve(process.cwd(), 'packages/demo')
+        }
+
+        if (!newRenderProjectDir) {
+            newRenderProjectDir = path.resolve(process.cwd(), `packages/${moudleName}`)
+        }
+
+        if (!fs.existsSync(newRenderProjectDir)) {
+            fs.mkdirpSync(newRenderProjectDir)
+        }
+        fs.copySync(templateDir, newRenderProjectDir)
+
+        // 渲染
+        require('./create-project/renderProject')(newRenderProjectDir)
+    } catch (err) {
+        throw new Error('渲染模版错误：', err.message)
+    }
+}
